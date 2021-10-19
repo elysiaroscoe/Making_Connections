624419784349e96a5ef269479df271483defceac
Roscoe <elysiaroscoe@gmail.com>
2021-10-19 16:54:12 -0700

    commit with edits

--- script.js
+++ script.js
@@ -16,16 +16,13 @@ function removePhil() {
 
 function respondRequest() {
     var element = document.querySelector("#numrequest")
-    request = 2;
-    request--;
-    element.innerText = request;
+    element.innerText--;
 }
 
 function acceptRequest(){
     var element = document.querySelector("#numconnect")
-    connect = 500;
-    connect++;
-    element.innerText = connect;
+    element.innerText++;
 }
 
-/* somehow i can only get the numbers to shift once. will refer to Marisa's code*/
+/* somehow i can only get the numbers to shift once. will refer to Marisa's code
+Okay i totally understand where she's coming from and she simplified it so much more nicely by using id and then specifying id in the html */
