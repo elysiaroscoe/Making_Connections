b9a97d5bafc45334eb39ae5b0d4d5123d96a90de
Roscoe <elysiaroscoe@gmail.com>
2021-10-19 16:43:58 -0700

    commit

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-var element = document.querySelector("#myname");
-
 function changeName() {
-
-    console.log(hi);
-    element.innerText = "Briana"
+    var element = document.querySelector("#myname");
+    element.innerText = "Fawna Deere"
 }
+
+function removeTodd() {
+    var element = document.querySelector("#todd")
+    element.remove();
+}
+
+function removePhil() {
+    var element = document.querySelector("#phil")
+    element.remove();
+}
+
+
+function respondRequest() {
+    var element = document.querySelector("#numrequest")
+    request = 2;
+    request--;
+    element.innerText = request;
+}
+
+function acceptRequest(){
+    var element = document.querySelector("#numconnect")
+    connect = 500;
+    connect++;
+    element.innerText = connect;
+}
+
+/* somehow i can only get the numbers to shift once. will refer to Marisa's code*/
